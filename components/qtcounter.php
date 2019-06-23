2e41f728ba9d8d19952ec8b338b5c4adfcac89ba
londhor <devero.devid@gmail.com>
2019-06-23 21:57:27 +0300

    Update

--- components/qtcounter.php
+++
@@ -1,9 +0,0 @@
-<template id="qtcounter">
-	<div class="input-count-box">
-		<button @click="down()" type="button" class="form-count-btn ic-m-minus minus"></button>
-		<div class="form-count-wp" data-text="шт.">
-			<input type="text" name="qt" v-model="qt" class="form-count" required>
-		</div>
-		<button @click="up()" type="button" class="form-count-btn ic-m-plus plus"></button>
-	</div>
-</template>
